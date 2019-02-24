8b387611164181366ba3d8dedf464d3f18a9e141
santosh2055 <santoshchapagain55@gmail.com>
2019-02-24 14:39:38 +0545

    initial push

---
+++ function.py
@@ -0,0 +1,18 @@
+def mul(x,y):
+    return x*y
+def div(x,y): 
+    return x/y
+def sub(x,y):
+    return x-y
+def rem(x,y):
+    return x%y
+def sum(x,y):ls
+    return x+y
+def cub(x):
+    return x*x*x
+def sqr(x):
+    return x*x    
+                  
+#x = int(input("enter the number"))
+#y = int(input("enter another number"))
+#print("multiple is,mul(x,y), "divison :",div(x,y), "subtration :",sub(x,y),"reminder :",rem(x,y), "sum :",sum(x,y),"cubic :",cub(x), "square :",sqr(x))
---
+++ main.py
@@ -0,0 +1,23 @@
+#import function
+#m = function.mul(2,3)
+#print("mul is ", m)
+#s = function.sum(2,6)
+#print("sum is ",s)
+
+# for math function   1.puthon3 2.import math 3.math. tab tab
+
+from function import sum
+s = sum(1,3)
+print("sum is",s)
+
+from function import div
+d = div(4,2)
+print("div is",d)
+
+from function import cub
+c = cub(4)
+print("cub is",c)
+
+from function import mul
+m = mul(4,2)
+print("multipaction is",m)
